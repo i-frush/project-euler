38925a4958d380a2efb2b034e6975b180e5df3ad
“i-frush” <a147473155@163.com>
2023-01-02 16:45:15 +0800

    2023.01.02

--- Difficulty5/problem57/pro57.py
+++ Difficulty5/problem57/pro57.py
@@ -3,3 +3,13 @@ import numpy as np
 import sys
 sys.path.append("D:\Documents\codePractice\eulerProject")
 
+
+n = sympy.Rational(1, 1)
+n.denominator
+ans = 0
+for i in range(0, 1000):
+    n = 1/(n+1)
+    n = 1 + n
+    if len(str(n.numerator)) > len(str(n.denominator)):
+        ans += 1
+print(ans)
--- Difficulty5/problem59/pro59.py
+++ Difficulty5/problem59/pro59.py
@@ -3,3 +3,19 @@ import numpy as np
 import sys
 sys.path.append("D:\Documents\codePractice\eulerProject")
 
+n = 1
+pnum = 0
+dnum = 1
+num = 1
+
+while True:
+    n += 2
+    d = n-1
+    dnum += 4
+    for i in range(0, 4):
+        num += d
+        pnum += 1 if sympy.isprime(num) else 0
+    
+    if pnum*10 < dnum:
+        print(n)
+        break
